448df885e9baf000b8cd4bee2819cb01003f2f1e
Masato Yamashita <m.yamashii@gmail.com>
2014-08-15 15:10:17 +0900

    Start trace over caliulator codes

--- StackCaliculator/main.c
+++ StackCaliculator/main.c
@@ -7,12 +7,25 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[])
-{
+#define STACK_MAX 10
 
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+/*配列によるスタック構造*/
+double stack[STACK_MAX];
+/*スタック頂上の位置(最下部からのオフセット)*/
+int stack_top = 0;
+
+
+/*スタックへのpush*/
+void stack_push(double val) {
+    if (stack_top == STACK_MAX) {
+        /*スタックが満杯になっている*/
+        printf("エラー:スタックが満杯です(Stack overflow)\n");
+        exit(EXIT_FAILURE);
+    }else{
+        /*渡された値をスタックに積む*/
+        stack[stack_top] = val;
+        stack_top++;
+    }
 }
-
