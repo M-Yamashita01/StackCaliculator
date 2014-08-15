32a742d1d126822e05348c3e5310298723f229fd
Masato Yamashita <m.yamashii@gmail.com>
2014-08-15 15:00:32 +0900

    Initial Commit

---
+++ StackCaliculator/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  StackCaliculator
+//
+//  Created by Yamashita Masato on 2014/08/15.
+//  Copyright (c) 2014年 Yamashita Masato. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
